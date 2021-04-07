d44ff4ee02cb7f2d6e991663df881f7a9ef21537
Remi Schwartz <schwartz.remi@gmail.com>
2021-04-07 14:55:49 +0200

    joke#index API

---
+++ app/controllers/api/v1/jokes_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::JokesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
+  def index
+    jokes = Joke.all
+    api_jokes = jokes.map { |joke| prepare_for_api(joke) }
+    render json: api_jokes
+  end
+
+  private
+
+  def prepare_for_api(joke)
+    {
+      id: joke.id,
+      author: joke.user.email,
+      language: joke.language.name,
+      category: joke.category.name,
+      content: joke.content,
+      created_at: joke.created_at
+    }
+  end
+end
