82d9acd42d736aa3abb6639b6d41b1dfd164cd2b
Remi Schwartz <schwartz.remi@gmail.com>
2021-05-03 10:18:55 +0200

    shorten brand when on mobile

--- app/javascript/jokes/components/navbar.tsx
+++ app/javascript/jokes/components/navbar.tsx
@@ -10,7 +10,8 @@ export default function NavBar({setCurrentPage}: Props) : JSX.Element {
   return (
     <nav className="flex justify-between items-center fixed inset-0 z-10 w-screen shadow-sm h-16 px-4 py-2 bg-yellow-600">
       <h1 className="text-3xl text-shadow text-white py-0 cursor-pointer" onClick={() => setCurrentPage('jokesIndex')}>
-          <span style={{fontFamily: 'Dancing Script'}}>Universal</span>
+          <span style={{fontFamily: 'Dancing Script'}}>Uni</span>
+          <span style={{fontFamily: 'Dancing Script'}} className="hidden sm:inline">versal</span>
           <span style={{fontFamily: 'Orelega One'}}>JOKES</span>
       </h1>
 
