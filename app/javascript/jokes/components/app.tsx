6b026e968ecbcf76c7cf16d1f176e93a2203b221
Remi Schwartz <schwartz.remi@gmail.com>
2021-05-10 14:38:57 +0200

    jokeList in Feed

--- app/javascript/jokes/components/app.tsx
+++ app/javascript/jokes/components/app.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Switch, useLocation }from 'react-router-dom';
+import { Route, Switch }from 'react-router-dom';
 
 import {PrivateRoute} from './private_route';
 import Feed from './feed';
@@ -9,41 +9,19 @@ import Profile from './profile';
 
 import {CurrentUserContext, defaultUser} from '../contexts/CurrentUserContext';
 import {getCurrentUser} from '../api/UserAPI';
-import {jokesIndex} from '../api/JokeAPI';
-import {savedJokesIndex} from '../api/SavedAPI';
 
 
 const App: React.FC = () => {
-  const [jokesList, setJokesList] = useState([]);
   const [currentUser, setCurrentUser] = useState(defaultUser);
-  const location = useLocation();
-
   useEffect(() => getCurrentUser(setCurrentUser), []);
-
-  useEffect(()=>{
-    switch (location.pathname) {
-      case "/":
-        jokesIndex(setJokesList);
-        break;
-      case "/saved":
-        savedJokesIndex(setJokesList);
-        break;
-    }
-  }, [location])
-
-
-  if (!jokesList) return <p>Loading...</p>;
 
   return (
     <CurrentUserContext.Provider value={currentUser} >
       <NavBar />
 
       <Switch>
-        <Route path="/" exact>
-          <Feed jokesList={jokesList} setJokesList={setJokesList} />
-        </Route>
-        <Route path="/saved">
-          <Feed jokesList={jokesList} setJokesList={setJokesList} />
+        <Route path={["/", "/saved"]} exact>
+          <Feed />
         </Route>
         <PrivateRoute path="/profile" setCurrentUser={setCurrentUser} component={Profile}/>
         <Route
--- app/javascript/jokes/components/feed.tsx
+++ app/javascript/jokes/components/feed.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useLocation} from 'react-router-dom';
 
 
 import {Joke} from './joke';
 import JokeForm from './joke_form';
-import {JokeObject} from '../api/JokeAPI';
+import {JokeObject, jokesIndex} from '../api/JokeAPI';
+import {savedJokesIndex} from '../api/SavedAPI';
 
 
-interface Props {
-  jokesList: JokeObject[];
-  setJokesList: React.Dispatch<React.SetStateAction<JokeObject[]>>;
-}
+export default function Feed(): JSX.Element {
+  const [jokesList, setJokesList] = useState<JokeObject[]>([]);
+  const location = useLocation();
 
-export default function Feed({jokesList, setJokesList}: Props): JSX.Element {
+  useEffect(()=>{
+    switch (location.pathname) {
+      case "/":
+        jokesIndex(setJokesList);
+        break;
+      case "/saved":
+        savedJokesIndex(setJokesList);
+        break;
+    }
+  }, [location])
+
+
   const renderJokesList = () => {
+    if (!jokesList) return <p className="mt-16">Loading...</p>;
+
     return jokesList.map ( joke => {
       return (
         <Joke joke={joke} key={joke.id} />
       );
     });
   }
+
 
   return (
     <main className='feed mt-16 p-4'>
