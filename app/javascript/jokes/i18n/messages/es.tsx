4e42ee2a5306fb8c769cca654a9ca5aa4251911c
Remi Schwartz <schwartz.remi@gmail.com>
2021-05-31 11:34:03 +0200

    translated to spanish

---
+++ app/javascript/jokes/i18n/messages/es.tsx
@@ -0,0 +1,49 @@
+import {LOCALES} from '../locales';
+
+export default {
+  [LOCALES.SPANISH]: {
+    "avatar_module.title": "Actualiza el avatar",
+    "avatar_module.select" : "Seleccionar una imagen",
+    "avatar_module.save" : "Registro",
+
+    "feed.no_joke": "Ningún chiste o usuario coincide (todavía) con esta búsqueda",
+
+    "joke.likes": "{nb_likes, plural, =0 {nadie se ha reído todavía, sé el primero} one {una persona se rio} other {# personas se rieron}}",
+    "joke.see_translations": "Ver las traducciones ({nb_translations})",
+
+    "joke_reaction.like": "Me he reído",
+    "joke_reaction.save": "Registro",
+    "joke_reaction.saved": "Registrado",
+    "joke_reaction.share": "Compartir",
+
+    "joke_form.placeholder": "¿Una pequeña broma para contarnos?",
+    "joke_form.submit": "Enviar",
+
+    "joke_show.translations": "Traducciones",
+    "joke_show.no_translations": "Aún no se ha ofrecido traducción en este idioma. ¡Es tu turno!",
+    "joke_show.filter": "Filtrar:",
+
+    "language_select.choose": "Elige idioma...",
+
+    "nav_items.login": "conectarse",
+
+    "not_connected.not_connected": "Aún no estás conectado",
+    "not_connected.continue": "Continuar sin iniciar sesión",
+    "not_connected.login": "Conectarse",
+    "not_connected.signup": "Crear una cuenta",
+
+    "profile.stats": "ESTADÍSTICAS",
+    "profile.app_language": "Idioma de la aplicación:",
+    "profile.jokes": "Chistes",
+    "profile.translations": "Traducciones",
+    "profile.likes": "Risas",
+    "profile.saved": "Registrado",
+    "profile.contactus": "Contáctenos",
+    "profile.logout": "Desconectarse",
+
+    "search_bar.search": "Buscar",
+
+    "translation_form.auto": "Auto",
+    "translation_form.send": "Enviar",
+  }
+}
--- app/javascript/jokes/i18n/messages/index.tsx
+++ app/javascript/jokes/i18n/messages/index.tsx
@@ -1,9 +1,11 @@
 import en from './en';
 import fr from './fr';
 import ja from './ja';
+import es from './es';
 
 export default {
   ...en,
   ...fr,
-  ...ja
+  ...ja,
+  ...es
 }
