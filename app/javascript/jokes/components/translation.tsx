aec2338f33c133300152917310a8659749898ce5
Remi Schwartz <schwartz.remi@gmail.com>
2021-05-31 11:43:48 +0200

    whitespace-pre-wrap for translation

--- app/javascript/jokes/components/translation.tsx
+++ app/javascript/jokes/components/translation.tsx
@@ -14,7 +14,7 @@ export function Translation({translation}: Props): JSX.Element {
     <div className="bg-white px-4 py-3 rounded-md border border-gray-200 shadow-sm mt-2">
       <AuthorBanner element={translation}/>
 
-      <div className="text-sm mt-2 ml-2">
+      <div className="whitespace-pre-wrap text-sm mt-2 ml-2">
         <p>{translation.content}</p>
       </div>
     </div>
